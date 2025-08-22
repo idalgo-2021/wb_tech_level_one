ea22fd99e0e4731f918dd9f4f9330e19fd36166d
idalgo-2021 <mr.samuelson@ya.ru>
2025-08-22 22:36:23 +0600

    task-1

--- cmd/task_1/main.go
+++ cmd/task_1/main.go
@@ -1,5 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() { fmt.Println("Hello task-1") }
+type Human struct {
+	Name string
+	Age  int
+}
+
+func (h *Human) Speak(msg string) {
+	fmt.Printf("%s: %s\n", h.Name, msg)
+}
+func (h *Human) Birthday() {
+	h.Age++
+}
+func (h Human) Info() string {
+	return fmt.Sprintf("Human{name=%q, age=%d}", h.Name, h.Age)
+}
+
+///////////
+
+type Action struct {
+	Human // embedded: методы Human наследуются в Action
+	Role  string
+}
+
+func (a *Action) Do(task string) {
+	fmt.Printf("%s (%s) что делает: %s\n", a.Name, a.Role, task)
+}
+
+// Можем переопределить
+// func (a *Action) Speak(msg string) {
+// 	fmt.Printf("[ACTION] %s (%s): %s\n", a.Name, a.Role, msg)
+// }
+
+func main() {
+	h := Human{Name: "Миша", Age: 29}
+	a := Action{
+		Human: h,
+		Role:  "Водитель",
+	}
+
+	fmt.Println(h.Info()) // Human{name="Миша", age=29}
+	fmt.Println(a.Info()) // Human{name="Миша", age=29}
+	fmt.Println("")
+
+	h.Birthday()
+	fmt.Println(h.Info()) // Human{name="Миша", age=30}
+	a.Birthday()
+	fmt.Println(a.Info()) // Human{name="Миша", age=30}
+	fmt.Println("")
+
+	a.Birthday()
+	fmt.Println(h.Info()) // Human{name="Миша", age=30}
+	fmt.Println(a.Info()) // Human{name="Миша", age=31}
+	fmt.Println("")
+
+	a.Do("рулит")
+	fmt.Println("") // Миша (Водитель) что делает: рулит
+
+	a.Speak("Привет from Action!")      // Миша: Привет from Action!
+	a.Human.Speak("Привет from Human!") // Миша: Привет from Human!
+	fmt.Println("")
+
+	a.Human.Name = "Michael"
+	fmt.Println(h.Info()) // Human{name="Миша", age=30}
+	fmt.Println(a.Info()) // Human{name="Michael", age=31}
+	fmt.Println("")
+
+	a.Name = "Михаил"
+	fmt.Println(h.Info()) // Human{name="Миша", age=30}
+	fmt.Println(a.Info()) // Human{name="Михаил", age=31}
+
+}
